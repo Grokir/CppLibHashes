0dca2602a37a38070f1ba9e8bf76f462922adff3
Grokir <grokir13@yandex.ru>
2025-09-08 21:07:29 +0300

    Add example for SHA-512

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,37 @@
 #include <cstdint>
 
 #include "SHA-2/sha256.hpp"
+#include "SHA-2/sha512.hpp"
 
 
 using namespace std;
 
 
-int main(){
-  SHA256 hasher;
-  string text = "Hello world! Test hash of SHA-256.";
-  
-  hasher(text);
+void SHA256_example();
+void SHA512_example();
 
-  cout << "sha-256('" << text << "') : " << hasher.hex() << endl;
+
+int main(){
+
+  SHA256_example();
+  SHA512_example();
 
   return 0;
+};
+
+
+void SHA256_example(){
+  SHA256 hasher;
+  string text = "Hello world! Test hash of SHA-256.";
+  hasher(text);
+
+  cout << "sha-256('" << text << "') : " << hasher.hex() << '\n' << endl;
+};
+
+void SHA512_example(){
+  SHA512 hasher;
+  string text = "Hello world! Test hash of SHA-512.";
+  hasher(text);
+
+  cout << "sha-512('" << text << "') : " << hasher.hex() << '\n' << endl;
 };
